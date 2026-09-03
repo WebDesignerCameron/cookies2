1107c5f5a3a272b1d237f3950a874cfbd6d754f3
WebDesignerCameron <webdesignercameron@gmail.com>
2026-09-03 17:07:22 +0100

    Add hero component

---
+++ app/components/index/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <section class="hero">
+            <div class="image-container">
+                <img src="../../assets/image/hero.png" />
+            </div>
+            <div class="content-container">
+                <h1>Cookie Clicker 🍪</h1>
+                <p>
+                    A fun Nuxt/Vue.js project
+                    with cool features and an
+                    interactive UI. Free,
+                    zero-ad cookie clicker
+                    with increment, clicks
+                    per second, and rebirthing
+                    features.
+                </p>
+            </div>
+        </section>
+    </main>
+</template>
